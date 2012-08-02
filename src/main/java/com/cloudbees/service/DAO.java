9243aeb47adb479cb74ecae01b4b39cf0d23a65c
mqprichard <mqprichard@gmail.com>
2012-08-01 17:23:04 -0700

    getGame() initial test

---
+++ src/main/java/com/cloudbees/service/DAO.java
@@ -0,0 +1,119 @@
+package com.cloudbees.service;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DAO {
+
+    String url = "java:comp/env/jdbc/mp_chess";
+    Connection conn;
+    Statement stmt;
+
+    public DAO() {
+    }
+
+    public void connect() {
+
+        try {
+            Context ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup(url);
+            conn = ds.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public ResultSet getGame(long game) {
+        ResultSet rst = null; 
+        try{
+        	String query = "select * from GAME where id = " + game;
+            stmt = conn.createStatement();
+            rst = stmt.executeQuery(query);
+        } catch (Exception e){
+            e.printStackTrace ();
+        } 
+        return rst;
+    }
+    
+    public ResultSet getBoard (long game){
+        ResultSet rst = null; 
+        try{
+         	String query = "select * from BOARD where game = " + game;     	
+            stmt = conn.createStatement();
+            rst = stmt.executeQuery(query);
+        } catch (Exception e){
+            e.printStackTrace ();
+        } 
+        return rst; 
+    }
+ 
+    public ResultSet getMoves (long game){
+        ResultSet rst = null; 
+        try{
+         	String query = "select * from MOVE where game_id = " + game;     	
+            stmt = conn.createStatement();
+            rst = stmt.executeQuery(query);
+        } catch (Exception e){
+            e.printStackTrace ();
+        } 
+        return rst; 
+    }
+    
+    public String newGame (String white, String black, String description){
+        String key = "";
+        String sql = "insert into GAME (white, black, description)"
+                   + "values (" + white +"," + black + "," + description + ")";
+        try{
+        	int rows = stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+         	ResultSet rs = stmt.getGeneratedKeys();
+         	if (rs.next()) {
+         		key = rs.getString(1);
+         	}
+        } catch (Exception e){
+            e.printStackTrace ();
+        } 
+        return key; 
+    }  
+    
+    public long newWhiteMove (long move, String white, long game){
+        long result = 0; 
+        try{
+         	String query = "insert into MOVES (move, white, game)"
+                         + "values (" + move + "," + white + "" + game + ")";
+            stmt = conn.createStatement();
+            result = stmt.executeUpdate(query);
+        } catch (Exception e){
+            e.printStackTrace ();
+        } 
+        return result; 
+    }  
+    
+    public long newBlackMove (long move, String black, long game) {
+    	long result = 0;
+        try{
+         	String query = "update MOVES set " + black
+                         + "where move = " + move + " and game = " + game;
+            stmt = conn.createStatement();
+            result = stmt.executeUpdate(query);
+        } catch (Exception e){
+            e.printStackTrace ();
+        } 
+        return result; 
+    }      	
+       
+    public void disconnect (){
+        try{
+            if (conn != null){
+                if (stmt != null) stmt.close ();
+                conn.close ();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/cloudbees/service/GameServlet.java
+++ src/main/java/com/cloudbees/service/GameServlet.java
@@ -1,17 +1,15 @@
 package com.cloudbees.service;
 
+import java.io.StringWriter;
+import java.sql.ResultSet;
+import javax.servlet.http.HttpServlet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceContextType;
-import javax.servlet.http.HttpServlet;
-
-import com.cloudbees.model.Game;
+import com.google.gson.stream.JsonWriter;
 
 
 @Path("/game")
@@ -19,22 +17,46 @@ public class GameServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
     
-	@PersistenceContext(unitName="chess", type=PersistenceContextType.TRANSACTION)
-	EntityManager entityManager;
-	Game game;
+//	@PersistenceContext(unitName="chess", type=PersistenceContextType.TRANSACTION)
+//	EntityManager entityManager;
 	
 	@GET
     @Path("{id}")	
 	@Produces(MediaType.APPLICATION_JSON)
-//	public String getGame(@PathParam("id") long id ) {
-//		return "{Game: }";
+	public String getGame(@PathParam("id") long id ) {
+		
+		StringWriter sw = new StringWriter();
+		JsonWriter writer = new JsonWriter(sw);
 
-	public Game getGame(@PathParam("id") long id ) {
 		try {
-		game = entityManager.find(Game.class, id);
+	       DAO dao = new DAO();
+	       dao.connect();
+
+	       ResultSet rst = dao.getGame(id);		   
+	       if (rst != null && rst.first()) {
+			   writer.beginObject();
+		       writer.name("id").value(rst.getInt(1));
+		       writer.name("white").value(rst.getString(2));	            	
+		       writer.name("black").value(rst.getString(3));
+		       writer.name("description").value(rst.getString(4));
+			   writer.endObject();
+			   writer.close();	       
+	       }  
+	       
+		   dao.disconnect();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return game;
-	}	
+	  
+	   return sw.toString();
+	}
+	
+//	public Game getGame(@PathParam("id") long id ) {
+//		try {
+//		game = entityManager.find(Game.class, id);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return game;
+//	}	
 }
