7f95f5bd2f18cc7ad71eeebf5d6c2cc730385961
mqprichard <mqprichard@gmail.com>
2012-08-09 17:38:16 -0700

    Added JUnit tests for DAO

--- src/main/java/com/cloudbees/service/DAO.java
+++ src/main/java/com/cloudbees/service/DAO.java
@@ -9,32 +9,36 @@ import javax.sql.DataSource;
 
 public class DAO {
 
-    String url = "java:comp/env/jdbc/mp_chess";
-    Connection conn;
-    Statement stmt;
+    private Connection conn;
+    private boolean isConnected;
+    private Statement stmt;
 
     public DAO() {
+    	isConnected = false;
     }
 
     public void connect() {
-
-        try {
+    	String url = "java:comp/env/jdbc/mp_chess";
+        
+    	try {
             Context ctx = new InitialContext();
             DataSource ds = (DataSource) ctx.lookup(url);
             conn = ds.getConnection();
-        } catch (Exception e) {
+            isConnected = true;   
+        } 
+    	catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     public ResultSet getGame(long game) {
         ResultSet rst = null; 
         try{
         	String query = "select * from GAME where id = " + game;
-            stmt = conn.createStatement();
+            stmt = getConnection().createStatement();
             rst = stmt.executeQuery(query);
-        } catch (Exception e){
+        } 
+        catch (Exception e){
             e.printStackTrace ();
         } 
         return rst;
@@ -42,11 +46,13 @@ public class DAO {
     
     public ResultSet getNext(long game) {
         ResultSet rst = null; 
+        
         try{
         	String query = "select next from GAME where id = " + game;
-            stmt = conn.createStatement();
+            stmt = getConnection().createStatement();
             rst = stmt.executeQuery(query);
-        } catch (Exception e){
+        } 
+        catch (Exception e){
             e.printStackTrace ();
         } 
         return rst;
@@ -54,11 +60,13 @@ public class DAO {
     
     public ResultSet getMoves (long game){
         ResultSet rst = null; 
+        
         try{
          	String query = "select * from MOVES where game = " + game;     	
-            stmt = conn.createStatement();
+            stmt = getConnection().createStatement();
             rst = stmt.executeQuery(query);
-        } catch (Exception e){
+        } 
+        catch (Exception e){
             e.printStackTrace ();
         } 
         return rst; 
@@ -68,15 +76,17 @@ public class DAO {
         long key = 0;
         String sql = "insert into GAME (white, black, description, next, move)"
                    + " values (\"" + white + "\",\"" + black + "\",\"" + description + "\",\"W\",\"1\")";
+        
         try{
-            stmt = conn.createStatement();
+            stmt = getConnection().createStatement();
         	int rows = stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
         	if (rows == 0) return key;
          	ResultSet rs = stmt.getGeneratedKeys();
          	if (rs.next()) {
          		key = rs.getLong(1);
          	}
-        } catch (Exception e){
+        } 
+        catch (Exception e){
             e.printStackTrace ();
         } 
         return key; 
@@ -91,7 +101,8 @@ public class DAO {
     	try {
     		stmt = conn.createStatement();
     		rows = stmt.executeUpdate(sql);
-    	} catch (Exception e){
+    	} 
+    	catch (Exception e){
     		e.printStackTrace ();
     	} 
     	return rows;     	
@@ -101,15 +112,17 @@ public class DAO {
         long key = 0; 
         String sql = "insert into MOVES (move, white, game)"
                    + "values (\"" + move + "\",\"" + white + "\",\"" + game + "\")";
+        
         try{
-         	stmt = conn.createStatement();
+         	stmt = getConnection().createStatement();
             int rows = stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
         	if (rows == 0) return key;
          	ResultSet rs = stmt.getGeneratedKeys();
          	if (rs.next()) {
          		key = rs.getLong(1);
          	}
-        } catch (Exception e){
+        } 
+        catch (Exception e){
             e.printStackTrace ();
         } 
 
@@ -118,12 +131,14 @@ public class DAO {
     
     public long newBlackMove (long move, String black, long game) {
     	long rows = 0;
-        try{
+        
+    	try{
          	String query = "update MOVES set black = \"" + black + "\"" 
                          + " where move = \"" + move + "\" and game = \"" + game + "\"";
-            stmt = conn.createStatement();
+            stmt = getConnection().createStatement();
             rows = stmt.executeUpdate(query);
-        } catch (Exception e){
+        } 
+        catch (Exception e){
             e.printStackTrace ();
         }
 
@@ -132,12 +147,25 @@ public class DAO {
        
     public void disconnect (){
         try{
-            if (conn != null){
+            if (conn != null) {
                 if (stmt != null) stmt.close ();
                 conn.close ();
+                isConnected = false;
             }
-        }catch (Exception e){
+        } 
+        catch (Exception e){
             e.printStackTrace();
         }
     }
+
+	public Connection getConnection() {
+        if (isConnected == false)
+        	connect();
+		return conn;
+	}
+
+	public void setConnection(Connection conn) {
+		this.conn = conn;
+	}
+
 }
---
+++ src/test/java/com/cloudbees/service/DAOTest.java
@@ -0,0 +1,132 @@
+package com.cloudbees.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import com.cloudbees.model.Game;
+import com.cloudbees.model.Move;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DAOTest {
+	Connection conn;
+	long key;
+	
+	DAO dao = new DAO() {
+		public void connect() {
+			  String url = "jdbc:mysql://ec2-75-101-156-134.compute-1.amazonaws.com:3306/mp_chess";
+//			  String dbName = "mp_chess";
+			  String driver = "com.mysql.jdbc.Driver";
+			  String userName = "queenbee"; 
+			  String password = "welcome1";
+			  try {
+				  Class.forName(driver).newInstance();
+				  conn = DriverManager.getConnection(url,userName,password);
+				  super.setConnection(conn);
+			  }
+			  catch(Exception e) {
+				  e.printStackTrace();
+			  }
+			  finally {			  
+			  }
+		}
+	};
+	
+	@Test
+	public void testNewGame() {
+		try {	
+			Game game = new Game();
+			
+			String testWhite = "Player White";
+			String testBlack = "Player Black";
+			String testDescription = "Match Description";
+		    String testResult = null;
+		    String testNext = "W";
+		    long testMove = 1;  
+			
+		    dao.connect();
+		    game.setWhite(testWhite);
+		    game.setBlack(testBlack);
+		    game.setDescription(testDescription);
+		    
+		    // Create a new game (key = game id)
+			key = dao.newGame( game.getWhite(),
+					           		game.getBlack(),
+					           		game.getDescription() );
+			assertFalse(key == 0);
+
+			ResultSet rst = dao.getGame(key);
+		    assertFalse(rst == null);
+
+		    rst.first();
+			assertEquals(key, rst.getInt(1));
+			assertEquals(testWhite, rst.getString(2));	            	
+			assertEquals(testBlack, rst.getString(3));
+			assertEquals(testDescription, rst.getString(4));
+			assertEquals(testResult, rst.getString(5));
+			assertEquals(testNext, rst.getString(6));
+			assertEquals(testMove, rst.getInt(7));
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			dao.disconnect();
+		}
+	}
+	
+	@Test
+	public void testMoves() {
+		try {
+			Move move = new Move();
+
+			// Create a new game: key is game id
+			testNewGame();
+			
+			String testWhite = "e2-e4";
+			String testBlack = "e7-e5";
+			long testMove = 1;
+			long rows = 0;
+		    			
+		    dao.connect();
+
+		    // Add white move #1
+		    move.setWhite(testWhite);
+		    move.setBlack("");
+		    move.setMove(testMove);
+		    move.setGame(key);
+		    
+		    rows = dao.newWhiteMove( move.getMove(), 
+					  		  		 move.getWhite(), 
+					  		  		 move.getGame());
+		    assertFalse(rows == 0);
+		    
+		    // Add black move #1
+		    move.setWhite("");
+		    move.setBlack(testBlack);
+		    
+		    rows = dao.newBlackMove( move.getMove(), 
+			  		  		  		 move.getBlack(), 
+			  		  		  		 move.getGame());
+		    assertFalse(rows == 0);
+		    
+		    // Check the row for move #1
+		    ResultSet rst = dao.getMoves(key);		   
+		    assertFalse(rst == null);
+
+		    rst.first();
+			assertEquals(testMove, rst.getInt(2));
+			assertEquals(testWhite, rst.getString(3));	            	
+			assertEquals(testBlack, rst.getString(4));	
+			assertEquals(key, rst.getInt(5));
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			dao.disconnect();
+		}
+	}	
+}
